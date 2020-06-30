f0b86c434e63edac19554f8a0c7f5509ae9d59a8
anastasiya <nastunef@mail.ru>
2020-06-30 23:50:18 +0700

    добавление в базу

--- currency_statistics/settings.py
+++ currency_statistics/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'currency_statistics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'currency_statistics',
+        'USER': 'power_girl',
+        'PASSWORD': 'Ntcnbhjdfybt_01',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
