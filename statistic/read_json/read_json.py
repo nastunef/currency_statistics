82e0071691003c53403a4e7f83c7b6ec7e078213
anastasiya <nastunef@mail.ru>
2020-07-01 16:38:54 +0700

    вывод статистики в виде графика

--- statistic/read_json/read_json.py
+++ statistic/read_json/read_json.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import requests
 from dateutil import parser
-from statistic.models import Statistics
+from statistic.models import *
 
 
 class ReadJson(object):
@@ -19,17 +19,17 @@ class ReadJson(object):
             self.js = self.get_json()
             js_data = self.get_date()
             if js_data.month == self.month and js_data.year == self.year:
-                usd = self.get_valuta_value('USD')
-                eur = self.get_valuta_value('EUR')
-                jpy = self.get_valuta_value('JPY')
-                cny = self.get_valuta_value('CNY')
                 # добавление в базу
-                st = Statistics( data=js_data, usd= usd, eur= eur, jpy= jpy, cny= cny)
+                st = Statistics(data= js_data, usd= self.get_valuta_value('USD'),
+                                eur= self.get_valuta_value('EUR'),
+                                jpy= self.get_valuta_value('JPY'),
+                                cny= self.get_valuta_value('CNY')
+                                )
                 st.save()
-                #получаем следующий url файла
-                self.url = 'https:' + self.js['PreviousURL']
             elif js_data.month < self.month and js_data.year <= self.year:
                 break
+            # получаем следующий url файла
+            self.url = 'https:' + self.js['PreviousURL']
 
     def get_json(self):
         return requests.get(self.url).json()
